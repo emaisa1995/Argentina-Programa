3f3b7c4dbaa7879b2c2f7e773a2547391c19502c
Emanuel Nieto <64280069+emaisa1995@users.noreply.github.com>
2022-10-14 14:22:37 -0300

    Create Ejercicio 15: Los premios.js
    El jurado de un torneo nos pidió la función medallaSegunPuesto que retorne la medalla que le corresponde a los primeros puestos, según la siguiente lógica:
    
    primer puesto: le corresponde "oro"
    segundo puesto: le corresponde "plata"
    tercer puesto: le corresponde "bronce"
    otros puestos: le corresponde "nada"

---
+++ Capítulo 2: Programación Imperativa/1. Funciones y tipos de datos/Ejercicio 15: Los premios.js
@@ -0,0 +1,10 @@
+function medallaSegunPuesto(puesto){
+  if (puesto === 1){
+    return "oro";
+  }if (puesto === 2){
+    return "plata";
+  }if (puesto ===3){
+    return "bronce";
+  }else return"nada";
+
+}
