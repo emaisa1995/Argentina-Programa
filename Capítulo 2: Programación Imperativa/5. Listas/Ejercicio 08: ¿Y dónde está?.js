f95bccd10ac5403c68dae77ccac1c16603e0d6a5
Emanuel Nieto <64280069+emaisa1995@users.noreply.github.com>
2022-10-19 23:04:41 -0300

    Create Ejercicio 08: ¿Y dónde está?.js
    Otra cosa que queremos hacer con las listas es saber en qué posición se encuentra un elemento. Para ello utilizamos la función posicion

---
+++ Capítulo 2: Programación Imperativa/5. Listas/Ejercicio 08: ¿Y dónde está?.js
@@ -0,0 +1,4 @@
+posicion(["a", "la", "grande", "le", "puse", "cuca"], "grande"); //devuelve 2
+
+let diasLaborales = ["lunes", "martes", "miercoles", "jueves", "viernes"]
+posicion(diasLaborales, "lunes"); //devuelve 0
