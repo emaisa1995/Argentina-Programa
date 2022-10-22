98530876f9068b98d330938183a1d5e0a6bc5a64
Emanuel Nieto <64280069+emaisa1995@users.noreply.github.com>
2022-10-21 21:26:07 -0300

    Create Ejercicio 02: Tu propio monumento.js
    Buenas habilidades de búsqueda! Los registros, al igual que las listas, son una estructura de datos porque nos permiten organizar información. Pero ¿en qué se diferencia un registro de una lista?
    
    En las listas podemos guardar muchos elementos de un mismo tipo que representen una misma cosa (por ejemplo todos números, o todos strings). No existen límites para las listas: pueden tener muchos elementos, ¡o ninguno!
    
    En un registro vamos a guardar información relacionada a una única cosa (por ejemplo un monumento o una persona), pero los tipos de los campos pueden cambiar. Por ejemplo, el nombre y la ubicación de un monumento son strings, pero su año de construcción es un número.

---
+++ Capítulo 2: Programación Imperativa/6. Registros/Ejercicio 02: Tu propio monumento.js
@@ -0,0 +1,3 @@
+let torreAzadi = { nombre: "Torre Azadi", locacion: "Teherán, Irán", anioDeConstruccion: 1971 };
+
+let monumentoNacionalALaBandera = { nombre: "Monumento Nacional a la Bandera", locacion: "Rosario, Argentina", anioDeConstruccion: 1957 };
