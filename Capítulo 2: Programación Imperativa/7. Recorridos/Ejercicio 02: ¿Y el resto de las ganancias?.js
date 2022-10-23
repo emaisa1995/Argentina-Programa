34a268289c4effd42c3cf1a1547361b566071882
Emanuel Nieto <64280069+emaisa1995@users.noreply.github.com>
2022-10-23 08:34:56 -0300

    Create Ejercicio 02: ¿Y el resto de las ganancias?.js

---
+++ Capítulo 2: Programación Imperativa/7. Recorridos/Ejercicio 02: ¿Y el resto de las ganancias?.js
@@ -0,0 +1,13 @@
+> gananciaTotal([
+      { mes: "enero", ganancia: 2 }, 
+      { mes: "febrero", ganancia: 3 }
+  ])
+> gananciaTotal([
+      { mes: "enero", ganancia: 2 }, 
+      { mes: "febrero", ganancia: 3 }, 
+      { mes: "marzo", ganancia: 1 }, 
+      { mes: "abril", ganancia: 8 }, 
+      { mes: "mayo", ganancia: 8 }, 
+      { mes: "junio", ganancia: -1 }
+  ])
+> gananciaTotal([])
