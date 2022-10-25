2dbe07b731d73bfe0a58cc21f059c8d259d8ef71
Emanuel Nieto <64280069+emaisa1995@users.noreply.github.com>
2022-10-25 01:05:15 -0300

    Create Ejercicio 06: Conociendo el país.rb
    Hasta ahora los métodos que vimos solo producían un efecto. Si bien solo pueden devolver una cosa, ¡pueden producir varios efectos!
    
    Solo tenés que poner uno debajo del otro de la siguiente forma:
    
    def self.comprar_libro!
        @plata -= 300
        @libros += 1
    end
    Como te dijimos, Pepita podía volar a diferentes ciudades. Y cuando lo hace, cambia su ciudad actual, además de perder 100 unidades de energía. Las distintas ciudades vas a poder verlas en la Biblioteca.
    
    Con esto en mente:
    
    Creá un atributo ciudad en Pepita: la ciudad donde actualmente está nuestra golondrina.
    Hacé que la ciudad inicial de pepita sea Iruya.
    Definí un método volar_hacia! en Pepita, que tome como argumento otra ciudad y haga lo necesario.
     ¡Dame una pista!
    Al parámetro de volar_hacia! tenés que darle un nombre. Podrías llamarlo ciudad, pero eso colisionaría con el nombre del atributo ciudad. Así que te proponemos otros nombres: una_ciudad o, mejor, destino;

---
+++ Capítulo 3: Programación con Objetos/2. Definiendo objetos: métodos y estado/Ejercicio 06: Conociendo el país.rb
@@ -0,0 +1,18 @@
+module Pepita
+  @energia = 100
+  @ciudad = Iruya
+
+  def self.volar_en_circulos!
+    @energia -= 10
+  end
+
+  def self.comer_lombriz!
+    @energia += 20
+  end
+
+  def self.volar_hacia!destino
+    @ciudad = destino
+    @energia -=  100
+  end
+
+end
