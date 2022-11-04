655e5ae5fda3e4ace21b924cd8d49fd6d2dadcbf
Emanuel Nieto <64280069+emaisa1995@users.noreply.github.com>
2022-11-04 02:22:54 -0300

    Create Ejercicio 01: ¿Pepita está feliz?.rb
    ¿Te acordás de Pepita? Bueno, aunque no lo creas, también cambia de estados de ánimo. En nuestro modelo de Pepita, vamos a representar simplemente dos estados posibles: cuando está débil y cuando está feliz.
    
    ¿Y cuándo ocurre eso?
    
    Pepita está débil si su energía es menor que 100.
    Pepita está feliz si su energía es mayor que 1000.
    Completá los métodos debil? y feliz? de Pepita.
    
    Como en esta lección no vamos a interactuar con las ciudades, hemos quitado todo lo relacionado a ellas de Pepita. Esto solo lo hacemos para que te sea más fácil escribir el código, no lo intentes en casa.

---
+++ Capítulo 3: Programación con Objetos/3. Polimorfismo y encapsulamiento/Ejercicio 01: ¿Pepita está feliz?.rb
@@ -0,0 +1,23 @@
+module Pepita
+  @energia = 1000
+
+  def self.energia
+    @energia 
+  end
+
+  def self.volar_en_circulos!
+    @energia -= 10
+  end
+  
+  def self.comer_alpiste!(gramos)
+    @energia += gramos * 15
+  end  
+
+  def self.debil?
+    self.energia < 100
+  end
+  
+  def self.feliz?
+    self.energia > 1000
+  end
+end
